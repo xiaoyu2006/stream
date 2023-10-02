5640456deab0608c75b4c82958eeae66f84a0422
Yi Cao <me@ycao.top>
2023-10-02 21:11:19 +0800

    Add prime sieve example

--- tests/main.lisp
+++ tests/main.lisp
@@ -21,4 +21,16 @@
     (defun fibgen (a b)
       (stream-cons a (fibgen b (+ a b))))
     (let ((fibs (fibgen 0 1)))
-      (ok (equal (stream->list (stream-first-n fibs 10)) '(0 1 1 2 3 5 8 13 21 34))))))
+      (ok (equal (stream->list (stream-first-n fibs 10)) '(0 1 1 2 3 5 8 13 21 34)))))
+  (testing "prime sieve infinite stream"
+    (defun divisible? (x y)
+        (zerop (mod x y)))
+    (defun sieve (s)
+      "Return a stream of sieved non-divisible elements from the given stream."
+      (stream-cons (stream-car s)
+                   (sieve (stream-filter (lambda (x) (not (divisible? x (stream-car s))))  ; filter out multiples of the first element
+                                         (stream-cdr s)))))
+    (defun stream-starting-from (n)
+      (stream-cons n (stream-starting-from (1+ n))))
+    (let ((primes (sieve (stream-starting-from 2))))
+      (ok (equal (stream->list (stream-first-n primes 10)) '(2 3 5 7 11 13 17 19 23 29))))))
